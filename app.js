ea1efd45ff24b3ead0ba5afb4de50dd3c8419ef4
Gabriel Felipe <gabrielfelipinho2006@gmail.com>
2026-04-07 15:50:29 -0400

    media com while

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /* instalar readline com npm
 npm install readline-sync, ou no terminal ou no chatbot 
 node_modules, package nunca sobe para o github, é só para o ambiente local, não precisa subir para o github 
-
+para comentar varioas lonhas shift + alt + a
 */
-const r = require('readline-sync'); // pode ser require, request etc... mas assim é mais fácil de escrever, e é o nome da variável que vai receber a biblioteca, pode ser qualquer nome, mas é comum usar a primeira letra da biblioteca, ou o nome completo, como readlineSync
+// const r = require('readline-sync'); // pode ser require, request etc... mas assim é mais fácil de escrever, e é o nome da variável que vai receber a biblioteca, pode ser qualquer nome, mas é comum usar a primeira letra da biblioteca, ou o nome completo, como readlineSync
 
 let nota;
-
+/* 
 do { 
     //executa o bloco
     nota = parseFloat(r.question("digite uma nota de 0 a 10: ")); // parse float torna ele um float
@@ -17,4 +17,29 @@ do {
        
 } while (nota < 0 || nota > 10); //guarda condição
 
-console.log("nota válida: " + nota);
+console.log("nota válida: " + nota); */
+const r = require('readline-sync');
+
+// variavel
+
+let soma = 0;
+let contador = 1;
+
+// contador para controlar o numero de notas
+while ( contador <= 5) {
+
+    // solicita a nota do usuário
+    let nota = parseFloat(r.question(`Nota ${contador}: `));
+
+    // realiza o calculo das notas, 4 notas.
+    soma += nota;
+
+    // incrementa o contador para a próxima nota
+    contador++;
+}
+
+// define a media a soma da 4 notas dividido por 4, para obter a média.
+let media = soma / 5;
+
+//printa a média e usa toFixed para limitar a 2 casas decimais.
+console.log("A média é: " + media.toFixed(2));
