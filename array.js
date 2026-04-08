81521593411ec9d16eff606644d4c23041f794e7
Gabriel Felipe <gabrielfelipinho2006@gmail.com>
2026-04-08 14:22:41 -0400

    teste array

---
+++ array.js
@@ -0,0 +1,21 @@
+const r = require('readline-sync');
+//lista de numeros de telefone
+/*let numero = [];
+
+ console.log(("faça a sua lista de numeros de telefone (com limite de 10 numeros)"));
+for (let i = 0; i < 10; i++) {
+    numero[i] = r.question("digite um numero de telefone: ");
+} 
+for (let i = 0; i < 10; i++) {
+    console.log(`numero de telefone ${i+1}: ${numero[i]}`);
+}  */
+
+// tabuada de multiplicação
+
+let num_tabuada;
+console.log("tabuada de multiplicação");
+console.log("insira um numero para ver a tabuada");
+num_tabuada=r.question("digite um numero: ");
+for (let i = 0; i <= 10; i++) {
+    console.log(`${num_tabuada} x ${i} = ${num_tabuada * i}`);
+}   
